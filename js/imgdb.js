ef2a9631c33b50a405930ad16666def828b6ddc1
Sowmya <sowmya@nyc>
2016-03-28 14:03:19 -0400

    working version

--- js/imgdb.js
+++ js/imgdb.js
@@ -1,22 +1,248 @@
-(function() {
-    var imgdb = {
+var imgdb = [{
+    "isDefault": true,
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0001.JPG",
+    "title": "Sunset in the Village",
+    "subtitle": "Landscape, Oil on Canvas",
+    "date": "Feb 2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0011.JPG",
+    "title": "Love is in the rain",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0014.JPG",
+    "title": "Smoke on Fire",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0018.JPG",
+    "title": "Making Headlines",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0020.JPG",
+    "title": "Ganesha",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0027.JPG",
+    "title": "Blue Bird on a Red Sky",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0031.JPG",
+    "title": "Wild Yellow Flowers",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0035.JPG",
+    "title": "Misty Meadows",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0038.JPG",
+    "title": "Because of you",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0041.JPG",
+    "title": "For me to you",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0044.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0048.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0049.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0050.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0053.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0056.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0058.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0062.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0064.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0068.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0072.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0074.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0077.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0078.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0079.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0081.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0082.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0084.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0085.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0091.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0092.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0093.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0095.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0101.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0109.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/paintings/DSC_0110.JPG",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2016"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/blog/wp-content/uploads/2014/03/DSC_0020.jpg",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2014 March"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/blog/wp-content/uploads/2014/03/DSC_0099.jpg",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2014 March"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/blog/wp-content/uploads/2014/03/DSC_0070.jpg",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2014 March"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/blog/wp-content/uploads/2013/08/IMG_0861.jpg",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2013 Aug"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/blog/wp-content/uploads/2013/04/Photo2281.jpg",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2013 Apr"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/blog/wp-content/uploads/2013/01/Photo2130.jpg",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2013 Jan"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/blog/wp-content/uploads/2012/12/Photo1975.jpg",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2012 Dec"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/blog/wp-content/uploads/2012/10/Photo1730.jpg",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2012 Oct"
+}, {
+    "imgpath": "http://www.sowmyakalyan.com/blog/wp-content/uploads/2012/10/Photo1720.jpg",
+    "title": "...",
+    "subtitle": "...",
+    "date": "2012 Oct"
+}];
 
-        db: [{
-            "url": "paintings/DSC_0001.JPG",
-            "title": "Sunset in the Village",
-            "subtitle": "Landscape",
-            "date": "Feb 2016"
-        }, {
-            "url": "paintings/DSC_0002.JPG",
-            "title": "Sunset in the Village",
-            "subtitle": "Landscape",
-            "date": "Feb 2016"
-        }],
 
-        init: function(e) {
-            
-        }
-    }
 
-    imgdb.init();
-})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
