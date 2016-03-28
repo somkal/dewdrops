aed511a989216a5d614dc1364275f44b0e9309ef
Sowmya <kalyan@Kalyans-Air.fios-router.home>
2016-03-28 08:27:00 -0400

    draft

---
+++ js/imgdb.js
@@ -0,0 +1,22 @@
+(function() {
+    var imgdb = {
+
+        db: [{
+            "url": "paintings/DSC_0001.JPG",
+            "title": "Sunset in the Village",
+            "subtitle": "Landscape",
+            "date": "Feb 2016"
+        }, {
+            "url": "paintings/DSC_0002.JPG",
+            "title": "Sunset in the Village",
+            "subtitle": "Landscape",
+            "date": "Feb 2016"
+        }],
+
+        init: function(e) {
+            
+        }
+    }
+
+    imgdb.init();
+})();
